a43f5978ed802ab4f6ce52135a010be8c6bebf50
Admin <admin@macbookpro.home>
2025-02-06 00:21:47 +0000

    Added internetgateway

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
 resource "aws_subnet" "public-sub1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/28"
